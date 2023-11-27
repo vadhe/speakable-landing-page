09ce1de82f5204cf96b65e88c7aa4496bf20b6d0
vadhe <aldyvadhe@gmail.com>
2023-11-27 21:40:20 +0800

    feat: fix window not found

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-import About from './components/landing-page/About';
+// import About from './components/landing-page/About';
 import { Blog } from './components/landing-page/Blog';
 import Features from './components/landing-page/Features';
 import { Metadata } from 'next';
 import { Header } from './components/landing-page/Header';
+import dynamic from 'next/dynamic'
+ 
+const About = dynamic(() => import('./components/landing-page/About'), {
+  loading: () => <p>Loading...</p>,
+})
+ 
 
 export const metadata: Metadata = {
   title: 'SpeakAble Youth',
